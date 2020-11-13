eba656eec13d18e9607853bb397e92f970ee2a5d
unknown <Hinduja@HTC-L-5CD01282M.HINDUJA-TECH.COM>
2020-11-14 03:33:40 +0530

    Initial version

--- cj-functional/src/cj_functional/core.clj
+++ cj-functional/src/cj_functional/core.clj
@@ -2,14 +2,11 @@
   (:require [clojure.spec.alpha :as s])
   (:import java.util.UUID))
 
-;; Validiator of cell values - can be blank, O or X
-(def blank-o-x? (s/or :blank #(= % :blank) :O #(= % :O) :X #(= % :X)))
-
-;; Board -> Relational data structure avoid hierarchy
+;; Players
 (def player1 :player1)
 (def player2 :player2)
 
-(defrecord Board [game-id game-status game-winner c11 c12 c13 c21 c22 c23 c31 c32 c33])
+(defrecord Game [game-id game-status game-winner c11 c12 c13 c21 c22 c23 c31 c32 c33])
 
 (defn new-board
   "New board, all cells set to :unplayed"
@@ -17,9 +14,9 @@
   (take 9 (repeat :unplayed)))
 
 (defn new-game
-  "Create a new game by combining a new board with in_process state and a new game UUID"
+  "Create a new game by combining a new board with in-process state and a new game UUID"
   []
-  (apply ->Board (conj (new-board) :none :in_process (UUID/randomUUID))))
+  (apply ->Game (conj (new-board) :none :in-process (UUID/randomUUID))))
 
 
 (defn str-game-row
@@ -30,6 +27,8 @@
 (defn print-game
   "Prints a game to console"
   [game]
+  (println "")
+  (println "-------------------------------")
   (println "Game " (:game-id game) ) 
   (println "Status: " (:game-status game) )
   (println "Winner: " (:game-winner game))
@@ -52,14 +51,14 @@
   "Check if any row/column/diagonal has won"
   [game]
   (cond
-    (check-row-win game :c11 :c12 :c13) (assoc game :game-status "Won" :game-winner (:c11 game))
-    (check-row-win game :c21 :c22 :c23) (assoc game :game-status "Won" :game-winner (:c21 game))
-    (check-row-win game :c31 :c32 :c33) (assoc game :game-status "Won" :game-winner (:c31 game))
-    (check-row-win game :c11 :c21 :c31) (assoc game :game-status "Won" :game-winner (:c11 game))
-    (check-row-win game :c12 :c22 :c32) (assoc game :game-status "Won" :game-winner (:c12 game))
-    (check-row-win game :c13 :c23 :c33) (assoc game :game-status "Won" :game-winner (:c13 game))
-    (check-row-win game :c11 :c22 :c33) (assoc game :game-status "Won" :game-winner (:c11 game))
-    (check-row-win game :c13 :c22 :c31) (assoc game :game-status "Won" :game-winner (:c13 game))
+    (check-row-win game :c11 :c12 :c13) (assoc game :game-status :Won :game-winner (:c11 game))
+    (check-row-win game :c21 :c22 :c23) (assoc game :game-status :Won :game-winner (:c21 game))
+    (check-row-win game :c31 :c32 :c33) (assoc game :game-status :Won :game-winner (:c31 game))
+    (check-row-win game :c11 :c21 :c31) (assoc game :game-status :Won :game-winner (:c11 game))
+    (check-row-win game :c12 :c22 :c32) (assoc game :game-status :Won :game-winner (:c12 game))
+    (check-row-win game :c13 :c23 :c33) (assoc game :game-status :Won :game-winner (:c13 game))
+    (check-row-win game :c11 :c22 :c33) (assoc game :game-status :Won :game-winner (:c11 game))
+    (check-row-win game :c13 :c22 :c31) (assoc game :game-status :Won :game-winner (:c13 game))
     (some #(= % :unplayed) (vals game)) game
     :else (assoc game :game-status "Draw" :game-winner :none)))
 
@@ -68,7 +67,7 @@
   "Play the player at the cell. 
    Silently ignore if illegal move of aleady played cell or illegal cell."
   [player cell game]
-  (if (= (cell game) :unplayed)
+  (if (and (= (cell game) :unplayed) (= (:game-status game) :in-process ))
     (check-win (assoc game cell player))
     game))
 
