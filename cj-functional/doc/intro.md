1d7dc068b86993989cc8115423324eaca0ea1e1d
unknown <Hinduja@HTC-L-5CD01282M.HINDUJA-TECH.COM>
2020-11-13 20:46:52 +0530

    OK, syncing up cj new app

---
+++ cj-functional/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cj-functional
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ cj-functional/test/cj_functional/core_test.clj
@@ -0,0 +1,7 @@
+(ns cj-functional.core-test
+  (:require [clojure.test :refer :all]
+            [cj-functional.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
